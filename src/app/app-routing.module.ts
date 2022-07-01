ffbcef8f1e3d723f179c87fec666e460f301192d
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-07-01 15:38:50 +0300

    added components
    Signed-off-by: Geo Maximus <pescarugeorgiana19@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { ManagerPageComponent } from './pages/manager-page/manager-page.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  { path: 'signup', component: SignupPageComponent},
+  { path: 'user', component: UserPageComponent},
+  { path: 'manager', component: ManagerPageComponent},
+  { path: '**', component: NotfoundPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserPageComponent } from './pages/user-page/user-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { FormComponent } from './components/form/form.component';
+import { ManagerPageComponent } from './pages/manager-page/manager-page.component';
+import { NotfoundPageComponent } from './pages/notfound-page/notfound-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserCardComponent,
+    FormComponent,
+    ManagerPageComponent,
+    NotfoundPageComponent,
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
