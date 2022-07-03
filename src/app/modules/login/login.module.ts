af60965e6e5bd9f1927a691d13792cfa55d863bf
madalinagabor <madalina.gabor@student.unitbv.ro>
2022-07-03 19:36:10 +0300

    Login and Register pages
    Signed-off-by: madalinagabor <madalina.gabor@student.unitbv.ro>

---
+++ src/app/modules/login/login.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { LoginRoutingModule } from './login-routing.module';
+import { LoginComponent } from 'src/app/components/login/login.component';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
+  ]
+})
+export class LoginModule { }
---
+++ src/app/modules/register/register.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RegisterRoutingModule } from './register-routing.module';
+import { RegisterComponent } from 'src/app/components/register/register.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [
+    CommonModule,
+    RegisterRoutingModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+  ]
+})
+export class RegisterModule { }
