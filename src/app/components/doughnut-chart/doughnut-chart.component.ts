e67eed0332f11ca06d53cd0e3e2c51407b50631a
Geo Maximus <pescarugeorgiana19@gmail.com>
2022-07-04 23:12:53 +0300

    changed modal and chart styles
    Signed-off-by: Geo Maximus <pescarugeorgiana19@gmail.com>

--- src/app/components/doughnut-chart/doughnut-chart.component.ts
+++ src/app/components/doughnut-chart/doughnut-chart.component.ts
@@ -14,23 +14,35 @@ export class DoughnutChartComponent implements OnInit {
     const myChart = new Chart("doughnut-chart", {
       type: 'doughnut',
       data: {
-        labels: ['Bad', 'So and so', 'Good', 'Very good', 'Amazing'],
+        labels: ['Bad', 'So and so', 'Good', 'Very good', 'Amazing', 'Perfect'],
         datasets: [{
           label: '# of Votes',
           data: [7, 17, 57, 37, 17, 27],
           backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)'
+            // 'rgba(255, 99, 132, 1)',
+            // 'rgba(54, 162, 235, 1)',
+            // 'rgba(255, 206, 86, 1)',
+            // 'rgba(75, 192, 192, 1)',
+            // 'rgba(153, 102, 255, 1)'
+            'rgba(12, 26, 156, 0.5)',
+            'rgba(136, 143, 215, 0.5)',
+            'rgba(12, 26, 156, 0.5)',
+            'rgba(93, 100, 184, 0.5)',
+            'rgba(12, 26, 156, 0.5)',
+            'rgba(136, 143, 215, 0.5)',
           ],
           borderColor: [
-            'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)'
+            // 'rgba(255, 99, 132, 1)',
+            // 'rgba(54, 162, 235, 1)',
+            // 'rgba(255, 206, 86, 1)',
+            // 'rgba(75, 192, 192, 1)',
+            // 'rgba(153, 102, 255, 1)'
+            'rgba(12, 26, 156, 0.5)',
+            'rgba(136, 143, 215, 0.5)',
+            'rgba(12, 26, 156, 0.5)',
+            'rgba(93, 100, 184, 0.5)',
+            'rgba(12, 26, 156, 0.5)',
+            'rgba(136, 143, 215, 0.5)',
           ],
           borderWidth: 1
         }]
